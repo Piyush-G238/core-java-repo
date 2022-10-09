4ecf444e574da859e8ade467c002365262ab033d
Piyush-G238 <guptapiyush238@gmail.com>
2022-10-09 14:46:12 +0530

    initialized java repo

---
+++ src/com/java/fundamentals/HelloWorld.java
@@ -0,0 +1,16 @@
+/*=================================================
+ * File : HelloWorld.java
+ * class : com.java.fundamentals.HelloWorld
+ * description : Program to print "Hello world" on the console
+ *=================================================
+ * s.no     date         user        description
+ *  1     09.10.2022    Piyush         created
+ * 
+ * ================================================*/
+package com.java.fundamentals;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
